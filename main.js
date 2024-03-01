a092fcc1fd6cb4c4a585c2c38e61a49a38ddaf4d
socarandang <socarandang@tlu.edu>
2024-03-01 16:25:36 -0600

    Worked on some bugs concerning responsiveness and animations

--- main.js
+++ main.js
@@ -1,58 +1,63 @@
 /*================== TOGGLE ICON NAVBAR ==============*/
-let menuIcon = document.querySelectorSelector('#menu-icon');
-let navbar = document.querySelector('.navbar')
+let menuIcon = document.querySelectorSelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-xmark');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
 };
 
 /*================== SCROLL SECTION ACTIVE LINK ==============*/
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach.apply(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[hred*=' + id + ']').classList.add('active');
-            });
-        };
-    });
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach.apply((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-    /*================== STICKY NAVBAR ==============*/
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window,scrollY > 100);
+  /*================== STICKY NAVBAR ==============*/
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    /*================== REMOVE TOGGLE ICON AND NAVBAR ==============*/
-    menuIcon.classList.remove('fa-xmark');
-    navbar.classList.remove('active');
+  /*================== REMOVE TOGGLE ICON AND NAVBAR ==============*/
+  menuIcon.classList.remove("fa-xmark");
+  navbar.classList.remove("active");
 };
 
 /*================== SCROLL REVEAL ==============*/
-ScrollReveal({ 
-    distance: '80px',
-    duration: 2000,
-    delay: 200,
- });
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
 
- ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
- ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'});
- ScrollReveal().reveal('.home-contact h1, about-img', {origin: 'left'});
- ScrollReveal().reveal('.home-contact p, about-content', {origin: 'right'});
+ScrollReveal().reveal(".home-content, heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
- /*================== TYPED JS ==============*/
-const typed = new Typed('.multiple-text', {
-    strings: ['Data Scientist', 'Data Engineer', 'Data Analyst'],
-    typeSpeed: 70,
-    backSpeed: 70,
-    backDelay: 1000,
-    loop: true,
+/*================== TYPED JS ==============*/
+const typed = new Typed(".multiple-text", {
+  strings: ["Data Scientist", "Data Engineer", "Data Analyst"],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
 });
