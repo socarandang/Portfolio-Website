27ff67c7591dd0cd2750df3297bc94017d2b4e63
socarandang <socarandang@tlu.edu>
2024-03-01 21:38:03 -0600

    Made some changes to make site prettier

--- main.js
+++ main.js
@@ -37,27 +37,3 @@ window.onscroll = () => {
   menuIcon.classList.remove("fa-xmark");
   navbar.classList.remove("active");
 };
-
-/*================== SCROLL REVEAL ==============*/
-ScrollReveal({
-  distance: "80px",
-  duration: 2000,
-  delay: 200,
-});
-
-ScrollReveal().reveal(".home-content, heading", { origin: "top" });
-ScrollReveal().reveal(
-  ".home-img, .services-container, .portfolio-box, .contact form",
-  { origin: "bottom" }
-);
-ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
-ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
-
-/*================== TYPED JS ==============*/
-const typed = new Typed(".multiple-text", {
-  strings: ["Data Scientist", "Data Engineer", "Data Analyst"],
-  typeSpeed: 70,
-  backSpeed: 70,
-  backDelay: 1000,
-  loop: true,
-});
